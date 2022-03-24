332374f9e489094d348652f45b027a4968eb7fe1
Nicha <ncqxmnt@gmail.com>
2022-03-25 01:20:58 +0700

    HelloSunshine

--- src/main/java/com/coolkidspace/linebot/LineBotController.java
+++ src/main/java/com/coolkidspace/linebot/LineBotController.java
@@ -89,26 +89,30 @@ public class LineBotController {
 
         if (work == true) {
             if (text.equals("m1")) {
-                this.reply(replyToken, new TextMessage("คุณเลือก\tm1\tThe Adam project"));
-                this.reply(replyToken, Arrays.asList(new TextMessage(
+                // this.reply(replyToken, new TextMessage("คุณเลือก\tm1\tThe Adam project"));
+                this.reply(replyToken, Arrays.asList(new TextMessage("คุณเลือก\tm1\tThe Adam project"),
+                        new TextMessage(
                         "เรื่องราวของ อดัม รี้ด (ไรอัน เรย์โนลด์ส) นักบินหนุ่มที่ได้เดินทางข้ามเวลา หลังจากประสบอุบัติเหตุยานตกในปี 2022 ก็ได้ย้อนเวลากลับไปและแอบเข้าไปในบ้านลึกลับแห่งหนึ่งจึงพบเข้ากับเด็กวัย 13 ปี และต่อมาก็ได้รับการเปิดเผยว่าเป็นร่างในวัยเด็กของตัวเขาเอง เขาทั้งสองจึงต้องร่วมมือกันตามหาพ่อ ( มาร์ค รัฟฟาโล ) พร้อมกับทำภารกิจกอบกู้อนาคต"),
                         new TextMessage("รีวิวแบบเต็มค่ะ\t\t\t : •	https://zhort.link/GdH"),
                         new TextMessage("Trailor : •	https://www.youtube.com/watch?v=nQm0wor_qoQ")));
             } else if (text.equals("m2")) {
-                this.reply(replyToken, new TextMessage("คุณเลือก\tm2\tThe misfits"));
-                this.reply(replyToken, Arrays.asList(new TextMessage(
+                // this.reply(replyToken, new TextMessage("คุณเลือก\tm2\tThe misfits"));
+                this.reply(replyToken, Arrays.asList(new TextMessage("คุณเลือก\tm2\tThe misfits"),
+                        new TextMessage(
                         "•	เรื่องราวของโจรอัจฉริยะอย่าง ริชาร์ด เพซ (เพียร์ซ บรอสแนน) ซึ่งแม้แต่คุกที่มีระบบการรักษาความปลอดภัยขั้นสูงสุดยังหยุดเขาไว้ไม่ได้ หลังจากหลบหนีการไล่ล่าของตำรวจ และเอฟบีไออย่างดุเดือด เพซได้เข้ามาเป็นส่วนหนึ่งของทีมโจรคนนอกคอกในนาม The Misfits โดยมี วิค (รับบทโดย ไมค์ พิรัชต์) เป็นเซียนระเบิดตัวอันตรายประจำทีม ทุกภารกิจของพวกเขาคือ การปล้นเอาคืนคนชั่วเพื่อช่วยเหลือคนถูกรังแก โดยไม่เปิดเผยตัวตน ซึ่งการได้หัวขโมยตัวพ่ออย่างเพซ มาร่วมทีมนำไปสู่ปฏิบัติการปล้นแห่งศตวรรษที่ทั้งลึกลับซับซ้อน และมาพร้อมกับแผนอันล้ำลึก เป้าหมายคือการโจรกรรมทองคำแท่งมูลค่านับล้านเหรียญจากชั้นใต้ดินของคุก ซึ่งมีระบบนิรภัยแน่นหนาที่สุดในโลก ทองคำแท่งเหล่านั้นเป็นท่อน้ำเลี้ยงของ ชัลต์ซ เจ้าพ่อธุรกิจมืดผู้อยู่เบื้องหลังแผนก่อการร้ายทั่วโลก"),
                         new TextMessage("รีวิวแบบเต็มค่ะ\t\t\t : •	https://zhort.link/GdA"),
                         new TextMessage("Trailor :•	https://www.youtube.com/watch?v=uIjCiU6ee8Q")));
             } else if (text.equals("m3")) {
-                this.reply(replyToken, new TextMessage("คุณเลือก\tm3\tRed notice"));
-                this.reply(replyToken, Arrays.asList(new TextMessage(
+                // this.reply(replyToken, new TextMessage("คุณเลือก\tm3\tRed notice"));
+                this.reply(replyToken, Arrays.asList(new TextMessage("คุณเลือก\tm3\tRed notice"),
+                        new TextMessage(
                         "•	นักวิเคราะห์อาชญากรของเอฟบีไอตามจับโจรขโมยงานศิลป์ที่ทางการต้องการตัวมากที่สุด แต่กลับกลายมาเป็นคู่หูเฉพาะกิจเพื่อล่าตัวอาชญากรหลบหนีที่นำหน้าอยู่ก้าวหนึ่งเสมอ"),
                         new TextMessage("รีวิวแบบเต็มค่ะ\t\t\t : •	https://www.sanook.com/movie/117465/"),
                         new TextMessage("Trailor : •	https://www.youtube.com/watch?v=Pj0wz7zu3Ms")));
             } else if (text.equals("m4")) {
-                this.reply(replyToken, new TextMessage("คุณเลือก\tm4\tThe divine fury"));
-                this.reply(replyToken, Arrays.asList(new TextMessage(
+                // this.reply(replyToken, new TextMessage("คุณเลือก\tm4\tThe divine fury"));
+                this.reply(replyToken, Arrays.asList(new TextMessage("คุณเลือก\tm4\tThe divine fury"),
+                        new TextMessage(
                         "•	ยงฮูเป็นนักมวยที่เก่งมากๆไม่เคยเเพ้ใครเเละเป็นคนที่ไม่เชื่อในพระเจ้าเนื่องมาจากปมในวัยเด็ก อยู่มาวันหนึ่งเขาก็มีแผลเป็นที่ฝ่ามือเหมือนพระเยซูซึ่งหมอก็ไม่สามารถให้คำตอบได้ สุดท้ายเขาจึงไปพบบาทหลวงเพื่อหวังจะรักษาแผล ไปๆมาๆด้วยความรู้สึกบางอย่างยงฮูก็ได้เข้าไปเป็นฝ่ายช่วย บาทหลวงอัน ที่เกือบตายมาจากการไล่ผีแทนเเละเขาก็เริ่มเรียนรู้พลังพิเศษที่สามารถขับไล่ปีศาจได้เเค่สัมผัสเเล้วราดน้ำมนต์ใส่(เหมือนเลือดของเขาเป็นไฟน้ำมนต์เป็นน้ำมัน) ทำให้ช่วยคนบริสุทธิ์ไว้ได้แต่เรื่องราวก็ไม่ได้ง่ายขนาดนั้นเพราะ จอมมารจีชินที่เป็นบาตรหลวงเหมือนกันเเต่กลับบูชาซาตานก็ได้มาขัดขวางเเละการต่อสู้อันดุเดือดก็ได้เริ่มขึ้น"),
                         new TextMessage("รีวิวแบบเต็มค่ะ\t\t\t : •	https://zhort.link/GdB"),
                         new TextMessage("Trailor : •	https://www.youtube.com/watch?v=jdvRbAG00p4")));
@@ -150,6 +154,8 @@ public class LineBotController {
                         new TextMessage("รีวิวแบบเต็มค่ะ\t\t\t : •	https://zhort.link/GdH"),
                         new TextMessage("Trailor : •	https://www.youtube.com/watch?v=nQm0wor_qoQ")));
             }
+        } else if (work == false){
+            this.reply(replyToken, new TextMessage("คุณต้องการอะไรคะ"));
         }
         return text;
     }
@@ -179,6 +185,11 @@ public class LineBotController {
             }
             case "รีวิว": {
                 this.reply(replyToken, new TextMessage("รีวิวหนัง หรือ รีวิวซีรีส์"));
+                break;
+            }
+            case "ยกเลิก": {
+                this.reply(replyToken, new TextMessage("รีวิวหนัง หรือ รีวิวซีรีส์"));
+                work = false;
                 break;
             }
             case "รีวิวหนัง": {
