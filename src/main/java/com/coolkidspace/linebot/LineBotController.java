743b174b2407b860d2427fd3a7ae17a277a5677a
Nicha <ncqxmnt@gmail.com>
2022-03-25 18:32:57 +0700

    goodevening

--- src/main/java/com/coolkidspace/linebot/LineBotController.java
+++ src/main/java/com/coolkidspace/linebot/LineBotController.java
@@ -25,7 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @LineMessageHandler
-public class LineBotController implements checkInput{
+public class LineBotController extends checkInput{
 
     // -------------------------Attribute-------------------------------------
     public String text;
@@ -50,41 +50,41 @@ public class LineBotController implements checkInput{
     }
 
     // checktext ที่ user input เข้ามา
-    public String checkText(String text) {
+    // public String checkText(String text) {
 
-        if ((text.equals("รีวว")) || (text.equals("รัวิว")) || (text.equals("รีววิ"))
-                || (text.equals("รัววิ")) || (text.equals("review")) || (text.equals("Review"))
-                || (text.equals("rEVIEW")) || (text.equals("eview")) || (text.equals("revirw"))
-                || (text.equals("Reviw")) || (text.equals("iu;b;")) || (text.equals("IU;B;"))) {
-            text = "รีวิว";
-        } else if ((text.equals("รีววหนัง")) || (text.equals("รัวิวหนัง")) || (text.equals("หนัง"))
-                || (text.equals("หนีง")) || (text.equals("movie")) || (text.equals("review movie"))
-                || (text.equals("Review movie")) || (text.equals("Review Movie")) || (text.equals("review Movie"))) {
-            text = "รีวิวหนัง";
-        } else if ((text.equals("รีววซีรีส์")) || (text.equals("รีววซีรีส์")) || (text.equals("รีวิววีรีส์"))
-                || (text.equals("รีวิวซีรีย์")) || (text.equals("รีวิวซีรี่")) || (text.equals("รีวิวซีรี่ส์"))
-                || (text.equals("ซีรี่ส์")) || (text.equals("ซีรีย์")) || (text.equals("ซีรี่"))
-                || (text.equals("ซีรี่ย์"))
-                || (text.equals("Review Series")) || (text.equals("Series")) || (text.equals("review Series"))
-                || (text.equals("รีวิววีรีส์"))) {
-            text = "รีวิวซีรีส์";
-        } else if ((text.equals("ยกเลิก")) || (text.equals("ยพเลิก")) || (text.equals("ยหเลิก"))
-                || (text.equals("ยดเลิก")) || (text.equals("ยปเลิก"))
-                || (text.equals("นกเลิก")) || (text.equals("บกเลิก"))
-                || (text.equals("ยกเลิด")) || (text.equals("ยกเลิเ")) || (text.equals("ยกเลิห"))
-                || (text.equals("ยกเลิพ")) || (text.equals("ยกเลิป"))
-                || (text.equals("Cancel")) || (text.equals("CANCEL")) || (text.equals("cancel"))) {
-            text = "ยกเลิก";
-            work = false;
-        } else if ((text.equals("ใช้ยังไง")) || (text.equals("ทำยังไง")) || (text.equals("วิธีใช้"))
-                || (text.equals("ขอวิธีใช้"))
-                || (text.equals("ใช้ยังงาย")) || (text.equals("How to use")) || (text.equals("ใช้เเบบไหน"))
-                || (text.equals("ใช้ไงอ่า"))
-                || (text.equals("ใช้ไง")) || (text.equals("เริ่มใช้")) || (text.equals("เริ่มต้น"))) {
-            text = "วิธีใช้งาน";
-        }
-        return text;
-    }
+    //     if ((text.equals("รีวว")) || (text.equals("รัวิว")) || (text.equals("รีววิ"))
+    //             || (text.equals("รัววิ")) || (text.equals("review")) || (text.equals("Review"))
+    //             || (text.equals("rEVIEW")) || (text.equals("eview")) || (text.equals("revirw"))
+    //             || (text.equals("Reviw")) || (text.equals("iu;b;")) || (text.equals("IU;B;"))) {
+    //         text = "รีวิว";
+    //     } else if ((text.equals("รีววหนัง")) || (text.equals("รัวิวหนัง")) || (text.equals("หนัง"))
+    //             || (text.equals("หนีง")) || (text.equals("movie")) || (text.equals("review movie"))
+    //             || (text.equals("Review movie")) || (text.equals("Review Movie")) || (text.equals("review Movie"))) {
+    //         text = "รีวิวหนัง";
+    //     } else if ((text.equals("รีววซีรีส์")) || (text.equals("รีววซีรีส์")) || (text.equals("รีวิววีรีส์"))
+    //             || (text.equals("รีวิวซีรีย์")) || (text.equals("รีวิวซีรี่")) || (text.equals("รีวิวซีรี่ส์"))
+    //             || (text.equals("ซีรี่ส์")) || (text.equals("ซีรีย์")) || (text.equals("ซีรี่"))
+    //             || (text.equals("ซีรี่ย์"))
+    //             || (text.equals("Review Series")) || (text.equals("Series")) || (text.equals("review Series"))
+    //             || (text.equals("รีวิววีรีส์"))) {
+    //         text = "รีวิวซีรีส์";
+    //     } else if ((text.equals("ยกเลิก")) || (text.equals("ยพเลิก")) || (text.equals("ยหเลิก"))
+    //             || (text.equals("ยดเลิก")) || (text.equals("ยปเลิก"))
+    //             || (text.equals("นกเลิก")) || (text.equals("บกเลิก"))
+    //             || (text.equals("ยกเลิด")) || (text.equals("ยกเลิเ")) || (text.equals("ยกเลิห"))
+    //             || (text.equals("ยกเลิพ")) || (text.equals("ยกเลิป"))
+    //             || (text.equals("Cancel")) || (text.equals("CANCEL")) || (text.equals("cancel"))) {
+    //         text = "ยกเลิก";
+    //         work = false;
+    //     } else if ((text.equals("ใช้ยังไง")) || (text.equals("ทำยังไง")) || (text.equals("วิธีใช้"))
+    //             || (text.equals("ขอวิธีใช้"))
+    //             || (text.equals("ใช้ยังงาย")) || (text.equals("How to use")) || (text.equals("ใช้เเบบไหน"))
+    //             || (text.equals("ใช้ไงอ่า"))
+    //             || (text.equals("ใช้ไง")) || (text.equals("เริ่มใช้")) || (text.equals("เริ่มต้น"))) {
+    //         text = "วิธีใช้งาน";
+    //     }
+    //     return text;
+    // }
 
     // check all code of movie or series that user input
     public String getTypeMovie(String replyToken, Event event, TextMessageContent content) {
@@ -318,26 +318,6 @@ public class LineBotController implements checkInput{
     public static class DownloadedContent {
         Path path;
         String uri;
-    }
-
-    @Override
-    public void getTypeMovie() {
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public boolean isWork() {
-        return work;
-    }
-
-    public void setWork(boolean work) {
-        this.work = work;
     }
 
 }
--- src/main/java/com/coolkidspace/linebot/checkInput.java
+++ src/main/java/com/coolkidspace/linebot/checkInput.java
@@ -1,7 +1,64 @@
 package com.coolkidspace.linebot;
 
-public interface checkInput{
-    public String checkText(String text);
-    public void getTypeMovie();
+public class checkInput{
+
+    public String text;
+    public boolean work =false;
+
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isWork() {
+        return work;
+    }
+
+    public void setWork(boolean work) {
+        this.work = work;
+    }
+
+    public String checkText(){
+        return text;
+    }
+   public String checkText(String text) {
+
+        if ((text.equals("รีวว")) || (text.equals("รัวิว")) || (text.equals("รีววิ"))
+                || (text.equals("รัววิ")) || (text.equals("review")) || (text.equals("Review"))
+                || (text.equals("rEVIEW")) || (text.equals("eview")) || (text.equals("revirw"))
+                || (text.equals("Reviw")) || (text.equals("iu;b;")) || (text.equals("IU;B;"))) {
+            text = "รีวิว";
+        } else if ((text.equals("รีววหนัง")) || (text.equals("รัวิวหนัง")) || (text.equals("หนัง"))
+                || (text.equals("หนีง")) || (text.equals("movie")) || (text.equals("review movie"))
+                || (text.equals("Review movie")) || (text.equals("Review Movie")) || (text.equals("review Movie"))) {
+            text = "รีวิวหนัง";
+        } else if ((text.equals("รีววซีรีส์")) || (text.equals("รีววซีรีส์")) || (text.equals("รีวิววีรีส์"))
+                || (text.equals("รีวิวซีรีย์")) || (text.equals("รีวิวซีรี่")) || (text.equals("รีวิวซีรี่ส์"))
+                || (text.equals("ซีรี่ส์")) || (text.equals("ซีรีย์")) || (text.equals("ซีรี่"))
+                || (text.equals("ซีรี่ย์"))
+                || (text.equals("Review Series")) || (text.equals("Series")) || (text.equals("review Series"))
+                || (text.equals("รีวิววีรีส์"))) {
+            text = "รีวิวซีรีส์";
+        } else if ((text.equals("ยกเลิก")) || (text.equals("ยพเลิก")) || (text.equals("ยหเลิก"))
+                || (text.equals("ยดเลิก")) || (text.equals("ยปเลิก"))
+                || (text.equals("นกเลิก")) || (text.equals("บกเลิก"))
+                || (text.equals("ยกเลิด")) || (text.equals("ยกเลิเ")) || (text.equals("ยกเลิห"))
+                || (text.equals("ยกเลิพ")) || (text.equals("ยกเลิป"))
+                || (text.equals("Cancel")) || (text.equals("CANCEL")) || (text.equals("cancel"))) {
+            text = "ยกเลิก";
+            work = false;
+        } else if ((text.equals("ใช้ยังไง")) || (text.equals("ทำยังไง")) || (text.equals("วิธีใช้"))
+                || (text.equals("ขอวิธีใช้"))
+                || (text.equals("ใช้ยังงาย")) || (text.equals("How to use")) || (text.equals("ใช้เเบบไหน"))
+                || (text.equals("ใช้ไงอ่า"))
+                || (text.equals("ใช้ไง")) || (text.equals("เริ่มใช้")) || (text.equals("เริ่มต้น"))) {
+            text = "วิธีใช้งาน";
+        }
+        return text;
+    }
 }
     
